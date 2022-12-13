1bf318088149615947151dfb58e607b3fd9db8a8
orikemary <orikemarychiderah@gmail.com>
2022-12-12 18:41:13 -0800

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		{
+
+			if (!((ones == tens) || (tens > ones)))/* eliminates repitition*/
+			{
+
+			putchar(tens);
+			putchar(ones);
+			if (!(ones == '0' && tens == '8'))/*add comma and space*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+}	}
+putchar('\n');
+
+return (0);
+}
