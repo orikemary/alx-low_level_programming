09cccf4c86e546318796891d26eef63448cc2e64
orikemary <orikemarychiderah@gmail.com>
2022-12-14 18:08:44 -0800

    100 times table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -check code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	prin-times_table(3);
+	_putchar('\n');
+	print_-times_table($);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar(\'n');
+	print_times_table(12);
+	return (0)
+}
